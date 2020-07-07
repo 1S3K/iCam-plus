c8fc701621c0c233e1c21ad4180261d83623bff5
Taehyeon Kim <taehyk528@gmail.com>
2020-07-08 03:35:12 +0900

    추가: 답변하기 모달창(1); 우선 질문 모달창과 같은 형식으로 제작; 과제 및 평가에 링크

--- project/src/static/js/modal.js
+++ project/src/static/js/modal.js
@@ -1,13 +1,23 @@
 const modal = document.querySelector(".modal");
+const answer = document.querySelector("#answer");
 
 const question_button = document.querySelector(".question_button");
+const comment_button = document.querySelector(".comment_button");
 
-const span = document.querySelector(".close");
+const close = document.querySelectorAll(".close");
 
 question_button.onclick = function () {
   modal.style.display = "block";
 };
 
-span.onclick = function () {
+comment_button.onclick = function () {
+  answer.style.display = "block";
+};
+
+close[0].onclick = function () {
   modal.style.display = "none";
+};
+
+close[1].onclick = function () {
+  answer.style.display = "none";
 };
