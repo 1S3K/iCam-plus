27cc9270c437913a6fc3c7246a6431bd6e024af8
Heedong Kim <ruthetum@gmail.com>
2020-07-13 20:53:16 +0900

    시간대에 따른 webex redirecting

--- project/src/static/js/modal.js
+++ project/src/static/js/modal.js
@@ -6,17 +6,36 @@ const comment_button = document.querySelector(".comment_button");
 const edit_button = document.querySelector(".editBtn");
 
 question_button.onclick = function () {
-  modal1.style.display = "block";
-  var player = videojs('sample-video');
+  if (question_button.innerHTML === "질문하기") {
+    modal1.style.display = "block";
+    var player = videojs('sample-video');
+    
+    player.pause();
+    localStorage.setItem("time", player.currentTime());
+
+    var min = parseInt(player.currentTime()/60);
+    var sec = parseInt(player.currentTime()%60);
+
+    document.getElementById('questionFormMin').value = min;
+    document.getElementById('questionFormSec').value = sec;
+  } else if ( question_button.innerHTML === "화상채팅 연결하기") {
+    var date = new Date();
+    var week = ['일', '월', '화', '수', '목', '금', '토'];
+    var curDay = date.getDay();
+    var curHour = date.getHours() % 12;
+    // var curMin = date.getMinutes();
+
+    if (document.getElementById('datedate').innerText === week[curDay]) {
+      if (parseInt(document.getElementById('stst').innerText) <= curHour && parseInt(document.getElementById('enen').innerText) >= curHour) {
+        window.open("https://skku-ict.webex.com/meet/smallkong", 'webex meeting');
+      } else {
+        alert('현재는 정해진 시간이 아닙니다.');
+      }
+    } else {
+      alert('현재는 정해진 요일이 아닙니다.');
+    }
+  }
   
-  player.pause();
-  localStorage.setItem("time", player.currentTime());
-
-  var min = parseInt(player.currentTime()/60);
-  var sec = parseInt(player.currentTime()%60);
-
-  document.getElementById('questionFormMin').value = min;
-  document.getElementById('questionFormSec').value = sec;
 };
 
 edit_button.onclick = function () {
