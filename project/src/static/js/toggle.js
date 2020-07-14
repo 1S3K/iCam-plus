26b3d3353eb4d0d3f71a83cdb756b5dea9701f9d
Taehyeon Kim <taehyk528@gmail.com>
2020-07-14 15:32:48 +0900

    수정완료: 공개/비공개 토글 버튼.

--- project/src/static/js/toggle.js
+++ project/src/static/js/toggle.js
@@ -1,11 +1,21 @@
-const toggleButton = document.querySelectorAll("#toggleButton");
+// const toggleButton = document.querySelectorAll("#toggleButton");
 
-toggleButton.addEventListener("click", () => {
-  if (this.innerText == "공개") {
-    this.innerHTML = "비공개";
-    this.classList.toggle(openNotOpen);
-  } else if (this.innerText == "비공개") {
-    this.innerHTML = "비공개";
-    this.classList.toggle(openNotOpen);
+// toggleButton.addEventListener("click", () => {
+//   if (this.innerText == "공개") {
+//     this.innerHTML = "비공개";
+//     this.classList.toggle(openNotOpen);
+//   } else if (this.innerText == "비공개") {
+//     this.innerHTML = "비공개";
+//     this.classList.toggle(openNotOpen);
+//   }
+// });
+
+$(".toggleButton").click(function () {
+  console.log("test");
+  if ($(this).text() == "공개") {
+    $(this).text("비공개");
+  } else if ($(this).text() == "비공개") {
+    $(this).text("공개");
   }
+  $(this).toggleClass("openNotOpen");
 });
