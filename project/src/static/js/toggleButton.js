676dfd5f021cf94cd98532723a10b37e0bd00a18
KangJunewoo <brianjune@naver.com>
2020-07-19 20:34:52 +0900

    기능설명 밑부분에도 토글버튼 추가

--- project/src/static/js/toggleButton.js
+++ project/src/static/js/toggleButton.js
@@ -2,6 +2,9 @@
   const b1 = document.getElementById('b1');
   const b2 = document.getElementById('b2');
   const b3 = document.getElementById('b3');
+  const b4 = document.getElementById('b4');
+  const b5 = document.getElementById('b5');
+  const b6 = document.getElementById('b6');
 
   const c1 = document.getElementById('c1');
   const c2 = document.getElementById('c2');
@@ -11,6 +14,10 @@
     b1.classList.add('btn-fill');
     b2.classList.remove('btn-fill');
     b3.classList.remove('btn-fill');
+    b4.classList.add('btn-fill');
+    b5.classList.remove('btn-fill');
+    b6.classList.remove('btn-fill');
+    
     
     c1.style.display='block';
     c2.style.display='none';
@@ -21,6 +28,9 @@
     b1.classList.remove('btn-fill');
     b2.classList.add('btn-fill');
     b3.classList.remove('btn-fill');
+    b4.classList.remove('btn-fill');
+    b5.classList.add('btn-fill');
+    b6.classList.remove('btn-fill');
     
     c1.style.display='none';
     c2.style.display='block';
@@ -31,9 +41,62 @@
     b1.classList.remove('btn-fill');
     b2.classList.remove('btn-fill');
     b3.classList.add('btn-fill');
+    b4.classList.remove('btn-fill');
+    b5.classList.remove('btn-fill');
+    b6.classList.add('btn-fill');
 
     c1.style.display='none';
     c2.style.display='none';
     c3.style.display='block';
+  });
+
+  b4.addEventListener('click', ()=>{
+    b1.classList.add('btn-fill');
+    b2.classList.remove('btn-fill');
+    b3.classList.remove('btn-fill');
+    b4.classList.add('btn-fill');
+    b5.classList.remove('btn-fill');
+    b6.classList.remove('btn-fill');
+    
+    
+    c1.style.display='block';
+    c2.style.display='none';
+    c3.style.display='none';
+    $('html, body').animate({
+      scrollTop:$('#c1').offset().top-150
+    }, 300);
+  });
+
+  b5.addEventListener('click', ()=>{
+    b1.classList.remove('btn-fill');
+    b2.classList.add('btn-fill');
+    b3.classList.remove('btn-fill');
+    b4.classList.remove('btn-fill');
+    b5.classList.add('btn-fill');
+    b6.classList.remove('btn-fill');
+    
+    c1.style.display='none';
+    c2.style.display='block';
+    c3.style.display='none';
+    $('html, body').animate({
+      scrollTop:$('#c2').offset().top-150
+    }, 300);
+  });
+
+  b6.addEventListener('click', ()=>{
+    b1.classList.remove('btn-fill');
+    b2.classList.remove('btn-fill');
+    b3.classList.add('btn-fill');
+    b4.classList.remove('btn-fill');
+    b5.classList.remove('btn-fill');
+    b6.classList.add('btn-fill');
+
+    c1.style.display='none';
+    c2.style.display='none';
+    c3.style.display='block';
+
+    $('html, body').animate({
+      scrollTop:$('#c3').offset().top-150
+    }, 300);
   });
 })();
